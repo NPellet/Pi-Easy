4d85482667a8262365a1ff196f8c324a43626bf5
Norman Pellet <norman.pellet@gmail.com>
2012-09-13 14:28:57 +0200

    New config

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,11 +1,11 @@
 <?php 
-define('NOM_SITE', 'Swiss Comedy Club');
-define('BASE_URL', 'http://www.swisscomedyclub.ch/admin/');
+define('NOM_SITE', 'Test du site');
+define('BASE_URL', 'http://localhost:8888/');
 define('DATA_ROOT_REL', 'upload/');
-define('FTP_ROOT_REL', 'upload/ftp/');
-define('DB_HOST', 'mysql.swisscomedyclub.ch');
-define('DB_USER', 'scc');
-define('DB_PASSWORD', '78q3gs1G');
-define('DB_NAME', 'swisscomedyclubch');
-define('MASTER_PASSWORD', '884d425dc59fc6c523a9bea095018f9d94a98724');
+define('FTP_ROOT_REL', 'ftp/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'admin1234');
+define('DB_NAME', 'dev');
+define('MASTER_PASSWORD', '4eed320a5854fea26d69508f3238a2a36488914e');
 ?>
